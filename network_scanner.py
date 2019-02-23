5fe19c144a035c0b4ae0252aa17ad3f4432c7581
root <root@localhost.localdomain>
2019-02-23 18:18:05 -0500

    Final version

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,16 @@
 #/usr/bin/python
 
 import scapy.all as scapy
+import optparse
+
+
+
+def provide_address():
+    #read command line arguments and provide the address
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--ip_address", dest="addr", help="Provide an ip/network address, ex: 192.168.1.1")
+    parser.add_option("-m", "--mask", dest="mask", help="Provide the mask, ex: /32")
+    return parser.parse_args()[0]
 
 def scan(ip_address):
     #Create a broadcast ARP packet
@@ -18,6 +28,11 @@ def print_result(result):
     for item in result:
         print(item["ip"] + "\t\t" + item["mac"])
     print("\n")
-print_result(scan("10.0.2.0/24"))
+
+
+address = provide_address()
+
+print_result(scan(address.addr + address.mask))
+
 
 
